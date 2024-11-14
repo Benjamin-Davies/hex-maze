bd173edb531cf596253c746cda865e6b795fd6c0
Benjamin Davies <bentendavies@gmail.com>
2024-11-14 23:44:24 +1300

    refactor: work with libc directly

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hex Maze
+
+**NOTE**: Only works on platforms with libc (Linux, MacOS, etc.) because I wanted to mess around and work with libc directly.
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,13 +1,14 @@
 use std::{
     io::{self, Read, Write},
+    mem,
     os::fd::AsRawFd,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
+    sync::atomic::{AtomicBool, Ordering},
 };
 
-use termios::{cfmakeraw, tcsetattr, Termios};
+use self::{
+    signal::{sigaction, SigAction, SigActionFlags, SigSet, Signal, SIGTERM},
+    termios::{cfmakeraw, tcgetattr, tcsetattr, Termios, TCSADRAIN},
+};
 
 pub const fn ctrl(c: u8) -> u8 {
     c & 0x1f
@@ -16,8 +17,9 @@ pub const fn ctrl(c: u8) -> u8 {
 pub const CTRL_C: u8 = ctrl(b'c');
 pub const ESC: u8 = ctrl(b'[');
 
+pub static EXIT: AtomicBool = AtomicBool::new(false);
+
 pub struct Terminal {
-    exit: Arc<AtomicBool>,
     stdin: io::StdinLock<'static>,
     stdout: io::StdoutLock<'static>,
     old_termios: Termios,
@@ -25,24 +27,35 @@ pub struct Terminal {
 
 impl Terminal {
     pub fn new() -> Self {
-        let exit = Arc::new(AtomicBool::new(false));
-        let exit_clone = exit.clone();
-        ctrlc::try_set_handler(move || {
-            exit_clone.store(true, Ordering::SeqCst);
-        })
-        .unwrap();
+        extern "C" fn sigkill_handler(_: Signal) {
+            EXIT.store(true, Ordering::SeqCst);
+        }
+        unsafe {
+            sigaction(
+                SIGTERM,
+                Some(&SigAction {
+                    handler: sigkill_handler,
+                    mask: SigSet::default(),
+                    flags: SigActionFlags::default(),
+                }),
+                None,
+            );
+        }
 
         let stdin = io::stdin().lock();
         let stdout = io::stdout().lock();
         let fd = stdin.as_raw_fd();
 
-        let old_termios = Termios::from_fd(fd).unwrap();
-        let mut termios = old_termios;
-        cfmakeraw(&mut termios);
-        tcsetattr(fd, termios::TCSADRAIN, &mut termios).unwrap();
+        let mut old_termios;
+        unsafe {
+            old_termios = mem::zeroed();
+            tcgetattr(fd, &mut old_termios);
+            let mut termios = old_termios;
+            cfmakeraw(&mut termios);
+            tcsetattr(fd, TCSADRAIN, &mut termios);
+        }
 
         let mut term = Self {
-            exit,
             stdin,
             stdout,
             old_termios,
@@ -59,13 +72,15 @@ impl Drop for Terminal {
         self.alt_screen(false).cursor_visible(true).flush();
 
         let fd = self.stdin.as_raw_fd();
-        tcsetattr(fd, termios::TCSADRAIN, &self.old_termios).unwrap();
+        unsafe {
+            tcsetattr(fd, TCSADRAIN, &self.old_termios);
+        }
     }
 }
 
 impl Terminal {
     pub fn should_exit(&self) -> bool {
-        self.exit.load(Ordering::SeqCst)
+        EXIT.load(Ordering::SeqCst)
     }
 
     pub fn read(&mut self) -> u8 {
@@ -95,7 +110,7 @@ impl Terminal {
         self.csi().write("2J")
     }
 
-    pub fn goto(&mut self, row: u32, col: u32) -> &mut Self {
+    pub fn goto(&mut self, row: u16, col: u16) -> &mut Self {
         self.csi();
         write!(self.stdout, "{row};{col}H").unwrap();
         self
@@ -116,5 +131,78 @@ impl Terminal {
         } else {
             self.csi().write("?1049l")
         }
+    }
+}
+
+/// A subset of signal.h
+mod signal {
+    use std::{ffi::c_int, mem};
+
+    #[derive(Clone, Copy)]
+    #[repr(transparent)]
+    pub struct Signal(c_int);
+
+    pub const SIGTERM: Signal = Signal(15);
+
+    #[repr(C)]
+    pub struct SigAction {
+        pub handler: SignalHandler,
+        pub mask: SigSet,
+        pub flags: SigActionFlags,
+    }
+
+    pub type SignalHandler = extern "C" fn(Signal);
+
+    #[derive(Default)]
+    #[repr(C)]
+    pub struct SigSet([u64; 1024 / (8 * mem::size_of::<u64>())]);
+
+    #[derive(Default)]
+    #[repr(transparent)]
+    pub struct SigActionFlags(c_int);
+
+    extern "C" {
+        pub fn sigaction(
+            signal: Signal,
+            action: Option<&SigAction>,
+            old_action: Option<&mut SigAction>,
+        ) -> c_int;
+    }
+}
+
+/// A subset of termios.h
+mod termios {
+    use std::{
+        ffi::{c_int, c_uint},
+        os::fd::RawFd,
+    };
+
+    #[derive(Clone, Copy)]
+    #[repr(C)]
+    pub struct Termios {
+        iflag: c_uint,
+        oflag: c_uint,
+        cflag: c_uint,
+        lflag: c_uint,
+        line: u8,
+        control_chars: [u8; 32],
+        ispeed: c_uint,
+        ospeed: c_uint,
+    }
+
+    #[derive(Clone, Copy)]
+    #[repr(transparent)]
+    pub struct TermiosSetAttrActions(c_int);
+
+    pub const TCSADRAIN: TermiosSetAttrActions = TermiosSetAttrActions(1);
+
+    extern "C" {
+        pub fn tcgetattr(fd: RawFd, termios: &mut Termios) -> c_int;
+        pub fn tcsetattr(
+            fd: RawFd,
+            optional_actions: TermiosSetAttrActions,
+            termios: &Termios,
+        ) -> c_int;
+        pub fn cfmakeraw(termios: &mut Termios);
     }
 }
